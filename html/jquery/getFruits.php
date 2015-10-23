3d097a0e79e6db2042a9b2985e6a37bd83a2446f
christine home <lang.christine7@gmail.com>
2015-10-23 15:22:52 +0200

    modifs 5e jour

---
+++ html/jquery/getFruits.php
@@ -0,0 +1,1 @@
+[{"Nom":"Pomme","Calorie":"40"},{"Nom":"Poire","Calorie":"56"},{"Nom":"Abricot","Calorie":"80"},{"Nom":"Kiwi","Calorie":"130"},{"Nom":"Mangue","Calorie":"800"},{"Nom":"Banane","Calorie":"200"},{"Nom":"Melon","Calorie":"340"}]
